0b7834fdd6a883a09c2f46283397937ae9d2c4e2
Charlie Dong <charlie.dong@tomra.com>
2019-12-31 10:44:54 +0100

    Add automatically deploy for vim related files

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,7 @@
+BASEDIR=$(dirname "$0")
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/.vim/ultisnips-customized
+cp $BASEDIR/*.snippets ~/.vim/ultisnips-customized/
+cp $BASEDIR/.vimrc* ~/
