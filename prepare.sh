34bcaae616fca80290e8a25c29f34ca89e7ab5a3
Charlie Dong <charlie.dong@tomra.com>
2020-01-06 07:22:37 +0100

    Add automated downloading customized snippets

--- prepare.sh
+++ prepare.sh
@@ -2,8 +2,7 @@ BASEDIR=$(dirname "$0")
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p ~/.vim/ultisnips-customized/UltiSnips
-cp $BASEDIR/*.snippets ~/.vim/ultisnips-customized/UltiSnips/
+git clone https://github.com/charlie-dong/ultisnips-customized.git ~/.vim/ultisnips-customized
 cp $BASEDIR/.vimrc* ~/
 cp $BASEDIR/.bashrc_proxy ~/
 echo -e "\nsource ~/.bashrc_proxy" >> ~/.bashrc
